57f68890bbb5de9617d7d91e2076acf6c936ca50
ikamirin <hiroaki_ikami@yahoo.co.jp>
2025-08-08 20:29:57 +0900

    fix variablesのインテンド

--- external_tables/asset_name_master/variables.tf
+++ external_tables/asset_name_master/variables.tf
@@ -1,10 +1,19 @@
 variable "project_id"  { type = string }
-variable "region"      { type = string  default = "asia-northeast1" }
-variable "location"    { type = string  default = "asia-northeast1" } # BigQuery のリージョン/マルチリージョン
+variable "region"      {
+    type = string
+    default = "asia-northeast1"
+    }
+variable "location"    {
+    type = string
+    default = "asia-northeast1"
+    } # BigQuery のリージョン/マルチリージョン
 variable "dataset_id"  { type = string }
 variable "table_id"    { type = string }
 variable "source_uris" { type = list(string) } # Google Sheets のURL（1つでも配列で）
-variable "sheet_range" { type = string  default = null } # "シート1!A:Z" など
+variable "sheet_range" {
+    type = string
+    default = null
+    } # "シート1!A:Z" など
 
 # BigQueryテーブルのスキーマ定義
 # 各列の名前、データ型、モード（必須/任意）、説明を定義
